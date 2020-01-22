69dc7107558d840d05c75d1fda13c44a9c86a157
lillux <lillux94@gmail.com>
2020-01-23 00:14:26 +0100

    retrying update

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+setup(name = 'morphoscanner',
+     version = '0.1',
+     description = 'A library to handle gromacs simulation data of Self Assembling Peptide',
+     url = 'https://github.com/lillux/sap_analysis',
+     author = 'Calogero Carlino, Federico Fontana',
+     author_email = 'calogero.carlino28@gmail.com',
+     license = 'GPLv3',
+     packages = find_packages())
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
