63367eb0e202e4c3d5401de1d2d2dc597442d354
lillux <lillux94@gmail.com>
2020-03-20 01:33:40 +0100

    fixed setup

--- setup.py
+++ setup.py
@@ -1,51 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(name = 'morphoscanner',
-     version = '0.1',
+     version = '0.0.1',
      description = 'A library to handle gromacs simulation data of Self Assembling Peptide',
      url = 'https://github.com/lillux/sap_analysis',
      author = 'Calogero Carlino, Federico Fontana',
      author_email = 'calogero.carlino28@gmail.com',
      license = 'GPLv3',
-     packages = find_packages(),
      zip_safe=False,
-     python_requires = '>= 3.6')
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
+     install_requires=['numpy>=1.18.1',
+                       'MDAnalysis>=0.20.1',
+                       'pandas>=1.0.1',
+                       'torch>=1.1.0',
+                       'networkx>=2.4'],
+                       
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Healthcare Industry',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+              
+      packages=find_packages())
