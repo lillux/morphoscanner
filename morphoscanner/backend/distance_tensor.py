6a56e6362a4f81ae8dc37c318cd457972b0e7add
lillux <lillux94@gmail.com>
2020-07-24 16:54:45 +0200

    cpython adds. Experimenting on jupyter

--- morphoscanner/backend/distance_tensor.py
+++ morphoscanner/backend/distance_tensor.py
@@ -5,7 +5,7 @@ Created on Thu Mar 19 20:11:57 2020
 """
 
 import torch
-from .topology import 
+#from .topology import 
 #import tqdm
 
  # instantiate 3d tensor with shape n_peptides * n_residues * n_dimension
