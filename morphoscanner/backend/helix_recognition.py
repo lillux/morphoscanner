Fixed alpha elix recognition function Added distance map calculation from pdb path in a single function

morphoscanner/backend/helix_recognition.py
@@ -9,30 +9,47 @@ import numpy as np
 def contact_map_helix_torch(distance_map):
     
     t_h_sub = distance_map.clone()
-    t_h_sub[t_h_sub<=4.7] = 0
-    t_h_sub[(t_h_sub>4.7) & (t_h_sub<=6)] = 2
-    t_h_sub[t_h_sub>6.5] = 3
+    t_h_sub[t_h_sub <= 4.4] = 0
+    t_h_sub[(t_h_sub > 4.4) & (t_h_sub<=5.1)] = 1
+    t_h_sub[(t_h_sub > 5.1) & (t_h_sub<=6.5)] = 2
+    t_h_sub[t_h_sub > 6.5] = 3
     
     return t_h_sub
 
 
-def contact_tracer_v1(contact_map):
+def contact_tracer(contact_map):
     contact_map = contact_map.clone()
-    second_type = 0
-    count_second = 0 
+    pace_helix = 0
+    count_alpha = 0
+    percentage_alpha = 0
+    count_beta  = 0
+    percentage_beta = 0
+    length_beta = 0
     folding = {}
+    explored = []
     for i in range(contact_map.shape[0]):
         for j in range(i+1, contact_map.shape[1]):
-            if contact_map[i][j] == 2:
-                if np.abs(j-i) == 3:
-                    count_second += 1
-                    second_type = (np.abs(j - i) * (count_second - 1) + second_type)/count_second
-
-    folding['pace_alpha311'] = second_type
-    folding['n_alpha311'] = count_second
-    if count_second <= 2:
-        return 0
-    else:
-        return folding
+            if (contact_map[i][j] == 2) & ((j in explored) == False):
+                if np.abs(j-i) in range(3,9):
+                    count_alpha += 1
+                    pace_helix = (np.abs(j - i) * (count_alpha - 1) + pace_helix)/count_alpha
+                    explored = np.append(explored,j)
+                    explored = np.append(explored,i)
+            elif (contact_map[i][j] == 1) & ((j in explored) == False):
+                if np.abs(j-i) > 3:
+                    count_beta += 1
+                    length_beta = (np.abs(j-i)*(count_beta - 1) + length_beta)/count_beta
+                    explored = np.append(explored,i)
+                    explored = np.append(explored,j)
+    folding['pace_helix'] = pace_helix
+    folding['n_carbonalpha'] = count_alpha
+    percentage_alpha = count_alpha/(count_alpha+count_beta)*100
+    folding['perc_alpha'] = percentage_alpha
+    folding['length_beta'] = length_beta
+    folding['n_carbonbeta'] = count_beta
+    percentage_beta = count_beta/(count_beta+count_alpha)*100
+    folding['perc_beta'] = percentage_beta
+    folding['explored'] = explored
+    return folding
     
     

morphoscanner/backend/pdb_read.py
@@ -5,6 +5,7 @@ Created on Wed Nov 18 19:55:01 2020
 """
 import numpy as np
 from timeit import default_timer as timer
+import backend
 
 
 from .distance import get_euclidean_distance
@@ -149,7 +150,7 @@ def test_distance_error(protein_dict):
     error = correct_distance - approximate_distance.cpu().numpy()
     
     # print number of distances on which statistics are calculated
-    print('Statistics are calculated on %d distances.\n' % error.size)
+    print('Statistics are calculated on %d measurements.\n' % error.size)
     
     # calculate and print statistics
     mae = np.mean(np.abs(error))
@@ -164,3 +165,25 @@ def test_distance_error(protein_dict):
     print('Standard deviation is: ', std)
     
     return
+
+
+def get_distance_map_from_pdb(path:str):
+    coordinate = backend.pdb_read.get_coordinate_from_pdb(path)
+    keys = [k for k in coordinate.keys()]
+    print('The file you opened has the following models ID:\n')
+    print(keys)
+    model = input('\nWrite the models that you want to select:')
+    if len(model)>0:
+        try:
+            model_coord = coordinate[model]
+            print('\nModel %s was selected' % str(model))
+            tensor = backend.pdb_read.get_coordinate_tensor_from_dict_single(model_coord)
+            distance_map = backend.distance_tensor.fast_cdist(tensor,tensor)
+        except KeyError:
+            print('%s is not a correct model ID.' % model)
+    else:
+        model = keys[0]
+        print('\nModel %s was selected.' % str(model))
+        tensor = backend.pdb_read.get_coordinate_tensor_from_dict_single(coordinate[model])
+        distance_map = backend.distance_tensor.fast_cdist(tensor,tensor)
+    return distance_map