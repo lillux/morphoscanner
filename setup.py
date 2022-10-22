4789732a6e7e79f7e520d2d0a4fab54a3d973845
lillux <lillux94@gmail.com>
2022-10-22 17:08:30 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name = 'morphoscanner',
-     version = '0.0.3_dev',
+     version = '0.0.4_dev',
      description = 'A library to handle Martini CG Gromacs trajectory data of Self Assembling Peptides',
      url = 'https://github.com/lillux/morphoscanner',
      author = 'Calogero Carlino, Federico Fontana',
