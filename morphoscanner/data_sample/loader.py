48d466ec927ee4fc278f803e2960a7bbb726a750
lillux <lillux94@gmail.com>
2022-11-20 02:58:07 +0100

    Fix path retrival for data_sample

--- morphoscanner/data_sample/loader.py
+++ morphoscanner/data_sample/loader.py
@@ -5,6 +5,11 @@ Created on Sun Nov 20 01:38:24 2022
 """
 import os
 import pathlib
+import importlib_resources
+import inspect
+import importlib
+import sys
+
 
 
 class dataloader:
@@ -13,11 +18,10 @@ class dataloader:
     '''
     
     def __init__(self, lib):
-        # save lib path as a str
-        self.current_path = os.path.dirname(lib.__file__)
-        # save .gro path as an os agnostic path object, then cast to str
-        self.gro_simple_str = str(pathlib.Path(self.current_path).joinpath('data_sample', 'simple_homogeneous', 'system.gro'))
-        # save .xtc path as an os agnostic path object, then cast to str
-        self.xtc_simple_str = str(pathlib.Path(self.current_path).joinpath('data_sample', 'simple_homogeneous', 'trajectory.xtc'))
+       # save lib path as a str
+        self.current_path = os.path.dirname(sys.argv[0])
+       # save .gro path as an os agnostic path object, then cast to str
+        self.gro_simple_str = str(pathlib.Path(self.current_path).joinpath('simple_homogeneous', 'system.gro'))
+       # save .xtc path as an os agnostic path object, then cast to str
+        self.xtc_simple_str = str(pathlib.Path(self.current_path).joinpath('simple_homogeneous', 'trajectory.xtc'))
         return
-    
